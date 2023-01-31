ce718f7051448126988f2a76d94cfb43fc848832
teddy004 <tewodrosatirsaw4@gmail.com>
2023-01-31 05:52:35 -0800

    adding different routings for multi pages

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Well come to the Home Page!</h1>"
+
+@app.route("/about/")
+def about():
+    return "<h1>Well come to about page</h1>"
